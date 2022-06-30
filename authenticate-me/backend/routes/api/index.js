4a95f412d6867787bcf8d9164512e02d42581c1e
fangruz114 <101694480+fangruz114@users.noreply.github.com>
2022-06-29 17:16:35 -0700

    test auth me and remove extra files

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -16,23 +16,23 @@ router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
 
-// // GET /api/set-token-cookie
-// router.get('/set-token-cookie', async (_req, res) => {
-//     const user = await User.findOne({
-//         where: {
-//             username: 'Demo-lition'
-//         }
-//     });
-//     setTokenCookie(res, user);
-//     return res.json({ user });
-// });
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+});
 
-// router.get('/restore-user', (req, res) => {
-//     return res.json(req.user);
-// });
+router.get('/restore-user', (req, res) => {
+    return res.json(req.user);
+});
 
-// router.get('/require-auth', requireAuth, (req, res) => {
-//     return res.json(req.user);
-// });
+router.get('/require-auth', requireAuth, (req, res) => {
+    return res.json(req.user);
+});
 
 module.exports = router;
