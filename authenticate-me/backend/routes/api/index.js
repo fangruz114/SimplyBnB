d4b8b2bf870d581e57a8603cc86924144f24fbf1
fangruz114 <101694480+fangruz114@users.noreply.github.com>
2022-06-29 17:01:40 -0700

    add auth me

---
+++ authenticate-me/backend/routes/api/index.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
+
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+router.use(restoreUser);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', function (req, res) {
+    res.json({ requestBody: req.body });
+});
+
+// // GET /api/set-token-cookie
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// });
+
+// router.get('/restore-user', (req, res) => {
+//     return res.json(req.user);
+// });
+
+// router.get('/require-auth', requireAuth, (req, res) => {
+//     return res.json(req.user);
+// });
+
+module.exports = router;
